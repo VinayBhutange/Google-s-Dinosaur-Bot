14dd3eaaf013f90b77761d127154558a1ddc6e05
Vinay Kumar Bhutange <vinaybhutange@gmail.com>
2018-07-03 12:32:21 +0530

    Removed bugs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Google-s-Dinosaur-Bot
- an automatic player bot for Google's dinosaur game and this technique can be used to make bot for any other game.
+ An automatic player bot for Google's dinosaur game and this technique can be used to make bot for any other game.
+ 1.for this we need 3 modules PILLOW,NUMPY,pyautogui
+ 2.Note down the x and y coordinate of image of dinsaur and tree 
+ 
---
+++ bot.py
@@ -0,0 +1,40 @@
+from PIL import ImageGrab
+from PIL import ImageOps
+import pyautogui
+import time
+from numpy import *
+
+class Coordinates():
+    replaybtn = (340,390)
+    dinosaur = (171,395)
+
+def restartGame():
+    pyautogui.click(Coordinates.replaybtn)
+    pyautogui.keyDown('down')
+
+def press_Space():
+    pyautogui.keyUp('down')
+    pyautogui.keyDown('space')
+    print("jump")
+    time.sleep(0.18)
+    pyautogui.keyUp('space')
+    pyautogui.keyDown('down')
+
+def image_grab():
+    box = (Coordinates.dinosaur[0]+60,Coordinates.dinosaur[1],Coordinates.dinosaur[0]+150,Coordinates.dinosaur[1]+5 )
+    image = ImageGrab.grab(box)
+    grayImage = ImageOps.grayscale(image)
+    a = array(grayImage.getcolors())
+    return a.sum()
+
+def main():
+    restartGame()
+    while True:
+        if(image_grab()!= 697):
+            press_Space()
+            time.sleep(0.1)
+
+main()
+
+
+
